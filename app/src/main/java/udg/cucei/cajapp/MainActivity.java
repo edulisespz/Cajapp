6a895963627add98b4603d41111fde1964eecd03
edulisespz <ed_ulisespz@hotmail.com>
2016-02-24 12:36:44 -0600

    start
    initasdas

---
+++ app/src/main/java/udg/cucei/cajapp/MainActivity.java
@@ -0,0 +1,17 @@
+package udg.cucei.cajapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "this is my Toast message!!! =)",
+                Toast.LENGTH_LONG).show();
+    }
+}
